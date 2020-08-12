c30698ac04ce951eeb6ee7a50d84e43437d92a7b
PedroNovaes25 <dodroxpedro28@gmail.com>
2020-08-12 13:42:10 -0300

    Testando a primeira classe domain - Categoria

---
+++ demospring2/src/main/java/com/teste2boot/demospring2/resources/domain/Categoria.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teste2boot.demospring2.resources.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author guita
+ */
+public class Categoria implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
