a57f26765464e8954ba319b93ac89efa8d93428b
PedroNovaes25 <dodroxpedro28@gmail.com>
2020-08-12 12:27:45 -0300

    Projeto criado

---
+++ demospring2/src/main/java/com/teste2boot/demospring2/Demospring2Application.java
@@ -0,0 +1,13 @@
+package com.teste2boot.demospring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospring2Application.class, args);
+	}
+
+}
