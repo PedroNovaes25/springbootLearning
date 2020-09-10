0cdec9c03db46cfe36ea308f0fb1eca84ed9f23a
PedroNovaes25 <dodroxpedro28@gmail.com>
2020-09-10 16:11:36 -0300

    criando o profile de desenvolvimento

---
+++ demospring2/src/main/java/com/teste2boot/demospring2/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.teste2boot.demospring2.config;
+
+import com.teste2boot.demospring2.services.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev") // essa configuração só será usada quando o profile "test" estiver sendo chamado
+public class DevConfig {
+
+    @Autowired
+    private DbService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
